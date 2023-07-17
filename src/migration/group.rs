addc757fd414415cb7bf40625611c91329ace38f
Eugene Lebedev <eugene.0x90@gmail.com>
2023-07-17 15:37:34 +0300

    Migration: create gitlab group on target instance if not exist

--- src/migration/group.rs
+++ src/migration/group.rs
@@ -32,3 +32,33 @@ pub fn get_group_list(client: &Client, instance: &InstanceConfig) -> anyhow::Res
         Err(anyhow!("unexpected server response"))
     }
 }
+
+pub fn create_gitlab_private_group(client: &Client, instance: &InstanceConfig,
+                               group_name: &str, path: &str) -> anyhow::Result<Vec<GitLabGroup>> {
+    info!("create group '{group_name}' at instance '{}'..", instance.host);
+
+    let url = format!("{}/api/v4/groups?name={}&path={}&visibility=private",
+                            instance.host, group_name, path);
+
+    let response = client.post(url)
+        .header(PRIVATE_TOKEN_HEADER, instance.token.to_string())
+        .send().context("gitlab api communication error")?;
+
+    let response_status = response.status();
+
+    if response_status == reqwest::StatusCode::OK {
+        let groups = response.json().context("unable to decode server response")?;
+
+        debug!("---[HTTP RESPONSE]----");
+        debug!("{:?}", groups);
+        debug!("---[/HTTP RESPONSE]----");
+
+        info!("group '{group_name}' has been created");
+
+        Ok(groups)
+
+    } else {
+        error!("unexpected server response code {}", response_status);
+        Err(anyhow!("unexpected server response"))
+    }
+}
--- src/migration/mod.rs
+++ src/migration/mod.rs
@@ -1,12 +1,19 @@
+use std::collections::HashMap;
+
 use anyhow::Context;
 use log::info;
 use reqwest::blocking::ClientBuilder;
 
 use crate::config::InstanceConfig;
-use crate::migration::group::get_group_list;
+use crate::migration::group::{create_gitlab_private_group, get_group_list};
 
 pub mod domain;
 pub mod group;
+
+pub struct GitLabGroupIdMap {
+    pub source_id: u32,
+    pub target_id: u32,
+}
 
 pub fn migrate_gitlab_instance(source: &InstanceConfig, target: &InstanceConfig) -> anyhow::Result<()> {
     info!("migrating groups and projects from '{}' to '{}'..", source.host, target.host);
@@ -16,11 +23,54 @@ pub fn migrate_gitlab_instance(source: &InstanceConfig, target: &InstanceConfig)
     let source_instance_groups = get_group_list(&client, &source)
         .context("unable to get gitlab groups from source instance")?;
 
+    let target_instance_groups = get_group_list(&client, &target)
+        .context("unable to get gitlab groups from target instance")?;
+
     info!("received groups from source instance:");
     info!("{:?}", source_instance_groups);
 
+    let mut group_map: HashMap<String, GitLabGroupIdMap> = HashMap::new();
+
     for group in source_instance_groups {
         info!("- group (id {}): '{}'", group.id, group.name);
+
+        let target_group_found = target_instance_groups.iter().find(|gg| gg.name == group.name);
+
+        match target_group_found {
+            Some(target_group) => {
+                group_map.insert(
+                    group.name.to_string(),
+                    GitLabGroupIdMap {
+                        source_id: group.id,
+                        target_id: target_group.id,
+                    }
+                );
+            }
+            None => {
+                let groups_resp = create_gitlab_private_group(&client, &target,
+                                            &group.name, &group.path)
+                                        .context("cannot create gitlab group on target instance")?;
+
+                if !groups_resp.is_empty() {
+                    let target_group = groups_resp.first().unwrap();
+
+                    group_map.insert(
+                        group.name.to_string(),
+                        GitLabGroupIdMap {
+                            source_id: group.id,
+                            target_id: target_group.id,
+                        }
+                    );
+                }
+            }
+        }
+
+        if group_map.contains_key(&group.name) {
+            let group_id_mapping = group_map.get(&group.name).unwrap();
+
+            unimplemented!()
+        }
+
     }
 
     Ok(())
